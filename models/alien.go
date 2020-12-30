0bc72670d38da6322375d37335e4541d953a5ef1
ChrisAmelia <>
2020-12-30 20:55:51 +0100

    test(Alien): moveDown()

--- models/alien.go
+++ models/alien.go
@@ -2,7 +2,35 @@ package models
 
 // Alien represents the invader.
 type Alien struct {
-	life uint
-	x, y uint
+	life int
+	x, y int
 	unicode string
+}
+
+// NewAlien returns an instance of Alien.
+func NewAlien(life, x, y int, unicode string) Alien {
+	alien := Alien{
+		life: life,
+		x: x,
+		y: y, 
+		unicode: unicode,
+	}
+
+	return alien
+}
+
+// DefaultAlien returns an instance of Alien
+// with default values set.
+func DefaultAlien() Alien {
+	alien := NewAlien(1, 0, 0, "V")
+
+	return alien
+}
+
+// moveDown moves the alien one square down
+// and returns its updated position.
+func (alien *Alien) moveDown() int {
+	alien.y -= 1
+
+	return alien.y
 }
---
+++ models/alien_test.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestAlienMoveDown tests method moveDown()
+// updates the alien's position.
+func TestAlienMoveDown(t *testing.T) {
+	alien := DefaultAlien()
+
+	alien.y = 7
+
+	alien.moveDown()
+
+	expected := 6
+	actual := alien.y
+
+	assert.Equal(t, expected, actual)
+}
